32a997e67420c995953a46e91649d539d9181994
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-17 14:08:05 +0900

    Remove unnecessary things

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,6 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as child_process from "child_process";
-import { runInThisContext } from "vm";
 import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(_: vscode.ExtensionContext) {
     vscode.languages.registerDocumentRangeFormattingEditProvider("coq", {
         provideDocumentRangeFormattingEdits(
