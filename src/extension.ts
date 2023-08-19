b82044009f80a447acb7de5ee5cee06e54fec761
Hiroki Tokunaga <tokusan441@gmail.com>
2023-08-19 13:36:43 +0900

    Add `coqfmt.args`

--- src/extension.ts
+++ src/extension.ts
@@ -5,17 +5,28 @@ export function activate(_: vscode.ExtensionContext) {
     vscode.languages.registerDocumentRangeFormattingEditProvider("coq", {
         provideDocumentRangeFormattingEdits(
             document: vscode.TextDocument,
-            range: vscode.Range,
+            range: vscode.Range
         ): vscode.ProviderResult<vscode.TextEdit[]> {
+            const config = vscode.workspace.getConfiguration("coqfmt");
+
+            const workspaceFullPath = vscode.workspace.getWorkspaceFolder(
+                document.uri
+            )?.uri.fsPath;
+
             try {
+                const cmd = "coqfmt ".concat(config.get("args", ""));
+
                 const formatted = child_process
-                    .execSync("coqfmt", { input: document.getText(range) })
+                    .execSync(cmd, {
+                        input: document.getText(range),
+                        cwd: workspaceFullPath,
+                    })
                     .toString();
 
                 return [vscode.TextEdit.replace(range, formatted)];
             } catch (e: any) {
                 vscode.window.showErrorMessage(
-                    `Failed to format the code: ${e.message}`,
+                    `Failed to format the code: ${e.message}`
                 );
 
                 return [];
